45ab345b60a361cacbd6e2a31afb2fa9b214183b
Omega Dziko F. <megaoy@yahoo.com>
2024-04-13 19:45:49 -0400

    bug solved

--- problems/recursive-debug.js
+++ problems/recursive-debug.js
@@ -12,10 +12,20 @@ doForAll([], (x) => x * 2); // => []
 doForAll([1, 2, 3], (x) => x + 1); // => [2, 3, 4]
 doForAll(["a", "b", "c"], (x) => x.toUpperCase()); // => ["A", "B", "C"]
 ***********************************************************************/
-
+debugger
 function doForAll(arr, action) {
+
+//to fix bug add a base case to work towards
+  if(arr.length === 0) {
+
+    return arr;
+  }
   return [action(arr[0]), ...doForAll(arr.slice(1), action)];
 }
+
+console.log(doForAll([], (x) => x * 2)); // => []
+console.log(doForAll([1, 2, 3], (x) => x + 1)); // => [2, 3, 4]
+console.log(doForAll(["a", "b", "c"], (x) => x.toUpperCase())); // => ["A", "B", "C"]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = doForAll;
